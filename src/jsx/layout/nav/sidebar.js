5cac49ab4884a81f28894052ee5eaa8709c9afd2
Abubakar Njumwa <auabake@gmail.com>
2022-06-09 08:52:39 +0300

    adding jsx

---
+++ src/jsx/layout/nav/sidebar.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDom from 'react-dom';
+
+const Sidebar = () => {
+    return (
+        <div> Hi There </div>
+    );
+};
+
+ReactDom.render(<App />, document.queryselector('#root'));
